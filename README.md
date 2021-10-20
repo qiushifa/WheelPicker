2f0c9190313c092f6ecf7513ceddcf1bf2ae8d7d
qiushifa <shifa.qiu@choiceform.com>
2021-10-20 17:21:16 +0800

    添加readme和示例代码

--- README.md
+++ README.md
@@ -1,46 +1,22 @@
-# Getting Started with Create React App
+# 利用tsx编写的纯粹的滚动选择器（支持滚动选择和点击选择）
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## 演示请看App.tsx代码
 
-## Available Scripts
+使用了tailwindcss，演示代码使用了materialUI
 
-In the project directory, you can run:
+## 注意
 
-### `yarn start`
+1，默认情况下，WheelPicker是不支持外部滚动的（即鼠标移出wheelpicker后就会不在响应滚动）
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+2，如果想要支持外部滚动（比如鼠标移动到modal视图时，仍想响应滚动），这时需要三步：
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+1）给WheelPicker添加ref
 
-### `yarn test`
+2）在WheelPicker的父级添加一个div，style样式参考例子（需要positon为fixed）
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+3）添加的div的滚动事件，执行上面ref组件的externalMove事件；鼠标松开时执行exterUp事件即可
 
-### `yarn build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import TestDrawer from "./TestDrawer";
 import WheelPicker from "./WheelPicker";
@@ -7,26 +7,8 @@ function App() {
   const [clickState, setClickState] = useState(false);
   // @ts-ignore
   const dataArr = [...Array(15).keys()].map((item) => String(item));
-  const modal = useRef(null);
-  const modal1 = useRef(null);
-
-  // useEffect(() => {
-  //   document.querySelector(".MuiBackdrop-root")?.addEventListener("mousemove", (e) => {
-  //     console.log("dd");
-  //     //@ts-ignore
-  //     modal.current.externalMove(e);
-  //     //@ts-ignore
-  //     modal1.current.externalMove(e);
-  //   });
-
-  //   document.querySelector(".MuiBackdrop-root")?.addEventListener("mouseup", (e) => {
-  //     //@ts-ignore
-  //     modal.current.externalUp();
-  //     //@ts-ignore
-  //     modal1.current.externalUp();
-  //   });
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // },[document.querySelector(".MuiBackdrop-root")]);
+  // const modal = useRef(null);
+  // const modal1 = useRef(null);
 
   return (
     <div className="">
@@ -38,9 +20,28 @@ function App() {
         弹出
       </button>
 
+      {/** 鼠标移出后不再滚动 */}
       <TestDrawer visible={clickState}>
         <div className="grid grid-cols-2 gap-x-2">
-          {/* <div
+          <WheelPicker
+            options={dataArr}
+            scrollEnd={(index, value) => {
+              console.log("@@", index, value);
+            }}
+          />
+          <WheelPicker
+            options={dataArr}
+            scrollEnd={(index, value) => {
+              console.log("@@", index, value);
+            }}
+          />
+        </div>
+      </TestDrawer>
+
+      {/** 鼠标移出后仍然可以滚动 */}
+      {/* <TestDrawer visible={clickState}>
+        <div className="grid grid-cols-2 gap-x-2">
+          <div
             className="grayLayer top-0 left-0 right-0 bottom-0 block fixed"
             onMouseMove={(e) => {
               //@ts-ignore
@@ -54,7 +55,7 @@ function App() {
               //@ts-ignore
               modal1.current.externalUp();
             }}
-          /> */}
+          />
 
           <WheelPicker
             options={dataArr}
@@ -62,7 +63,7 @@ function App() {
               console.log("@@", index, value);
             }}
             //@ts-ignore
-            // ref={modal}
+            ref={modal}
           />
           <WheelPicker
             options={dataArr}
@@ -70,10 +71,10 @@ function App() {
               console.log("@@", index, value);
             }}
             // @ts-ignore
-            // ref={modal1}
+            ref={modal1}
           />
         </div>
-      </TestDrawer>
+      </TestDrawer> */}
     </div>
   );
 }
